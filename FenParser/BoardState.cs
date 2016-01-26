9d186775e3cbc82f13b36da62906f1cecff8b1d4
alexqfredrickson <afredrickson@AFREDRIDKSON2.aecio.com>
2016-01-26 16:39:07 -0500

    initial commit
    Initial commit

---
+++ FenParser/BoardState.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FenParser
+{
+
+    /// <summary>
+    /// Contains data about the board state.
+    /// </summary>
+    public class BoardState
+    {
+        public List<string[]> Ranks { get; set; } // reversed; from rank #8 to rank #1
+        public string ActivePlayer { get; set; }
+        public bool WhiteCanKingsideCastle { get; set; }
+        public bool WhiteCanQueensideCastle { get; set; }
+        public bool BlackCanKingsideCastle { get; set; }
+        public bool BlackCanQueensideCastle { get; set; }
+        public string EnPassantSquare { get; set; }
+        public int? HalfMoveCounter { get; set; }
+        public int? FullMoveNumber { get; set; }
+
+        /// <summary>
+        /// Prints a graphical representation of the board state to the console.
+        /// </summary>
+        public void ToConsole()
+        {
+            Console.WriteLine("-----------------");
+
+            for (int i = 0; i < 8; i++)
+            {
+                string[] currentRank = this.Ranks.ElementAt(i);
+
+                Console.WriteLine(
+                      "|" + currentRank[0] + "|" + currentRank[1] + "|" + currentRank[2] + "|" + currentRank[3]
+                    + "|" + currentRank[4] + "|" + currentRank[5] + "|" + currentRank[6] + "|" + currentRank[7] + "|");
+                Console.WriteLine("-----------------");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The active player is " + this.ActivePlayer + ".");
+
+            if (this.WhiteCanKingsideCastle && this.WhiteCanQueensideCastle)
+            {
+                Console.Write("White can castle both king-side and queen-side, and ");
+            }
+            else if (this.WhiteCanKingsideCastle)
+            {
+                Console.Write("White can only castle on the king-side, and ");
+            }
+            else if (this.WhiteCanQueensideCastle)
+            {
+                Console.Write("White can only castle on the queen-side, and ");
+            }
+            else
+            {
+                Console.Write("White cannot castle, and ");
+            }
+
+            if (this.BlackCanKingsideCastle && this.BlackCanQueensideCastle)
+            {
+                Console.Write("Black can castle both king-side and queen-side.");
+            }
+            else if (this.BlackCanKingsideCastle)
+            {
+                Console.Write("Black can only castle on the king-side.");
+            }
+            else if (this.BlackCanQueensideCastle)
+            {
+                Console.Write("Black can only castle on the queen-side.");
+            }
+            else
+            {
+                Console.Write("Black cannot castle.");
+            }
+
+            Console.WriteLine();
+
+            if (String.IsNullOrEmpty(this.EnPassantSquare))
+            {
+                Console.WriteLine("There are no squares eligible for en-passant capture.");
+            }
+            else
+            {
+                Console.WriteLine(this.EnPassantSquare + " is eligible for en-passant capture.");
+            }
+
+            if (this.HalfMoveCounter != null)
+            {
+                if (this.HalfMoveCounter == 1)
+                {
+                    Console.WriteLine("There has been 1 halfmove since the last pawn advance or capture.");
+                }
+                else if (this.HalfMoveCounter == 0)
+                {
+                    Console.WriteLine("This is the first halfmove since the last pawn advance or capture.");
+                }
+                else
+                {
+                    Console.WriteLine("There have been " + this.HalfMoveCounter + " moves since the last pawn advance or capture.");
+                }
+            }
+
+            if (this.FullMoveNumber != null)
+            {
+                if (this.FullMoveNumber == 1)
+                {
+                    Console.WriteLine("This is the first move.");
+                }
+                else
+                {
+                    Console.WriteLine("This is move number " + this.FullMoveNumber + ".");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public BoardState()
+        {
+            Ranks = new List<string[]>();
+        }
+
+    }
+}
---
+++ FenParser/FenParser.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FenParser
+{
+    /// <summary>
+    /// Determines board state from a given FEN string.
+    /// </summary>
+    public class FenParser
+    {
+        public FenString FenString { get; set; }
+
+        /// <summary>
+        /// Parses a FEN string and returns data related to the board state.
+        /// </summary>
+        /// <returns>Returns board state metadata.</returns>
+        public BoardState Parse()
+        {
+            BoardState board = new BoardState();
+
+            board.Ranks = this.GetPiecePlacement(this.FenString.PiecePlacement);
+            board.ActivePlayer = this.GetActiveColor(this.FenString.ActiveColor);
+            board.WhiteCanKingsideCastle = this.GetWhiteQueensideCastlingAvailability(this.FenString.CastlingAvailability);
+            board.WhiteCanQueensideCastle = this.GetWhiteQueensideCastlingAvailability(this.FenString.CastlingAvailability);
+            board.BlackCanKingsideCastle = this.GetBlackKingsideCastlingAvailability(this.FenString.CastlingAvailability);
+            board.BlackCanQueensideCastle = this.GetBlackQueensideCastlingAvailability(this.FenString.CastlingAvailability);
+
+            board.EnPassantSquare = this.GetEnPassantSquare(this.FenString.EnPassantSquare);
+            board.HalfMoveCounter = this.GetHalfmoveClock(this.FenString.HalfmoveClock);
+            board.FullMoveNumber = this.GetFullmoveNumber(this.FenString.FullmoveNumber);
+
+            return board;
+        }
+
+        public List<string[]> GetPiecePlacement(string fenSubstring)
+        {
+            List<string[]> ranks = new List<string[]>();
+
+            string[] fenRanks = fenSubstring.Split('/');
+
+            for (int i = 0; i < 8; i++)
+            {
+                string[] fenRank = Array.ConvertAll(fenRanks[i].ToCharArray(), x => x.ToString());
+                List<string> newRank = new List<string>();
+
+                for (int j = 0; j < fenRank.Length; j++)
+                {
+                    string currentFenSquare = fenRank[j];
+
+                    int n;
+                    if (int.TryParse(currentFenSquare, out n))
+                    {
+                        int fenNullSquares = int.Parse(currentFenSquare);
+
+                        for (int k = 0; k < fenNullSquares; k++)
+                        {
+                            newRank.Add(" ");
+                        }
+                    }
+                    else
+                    {
+                        newRank.Add(currentFenSquare);
+                    }
+                }
+
+                ranks.Add(newRank.ToArray<string>());
+            }
+
+            return ranks;
+        }
+
+        public string GetActiveColor(string fenSubstring)
+        {
+            if (fenSubstring.ToLower().Equals("b"))
+            {
+                return "Black";
+            }
+            else if (fenSubstring.ToLower().Equals("w"))
+            {
+                return "White";
+            }
+
+            return String.Empty;
+        }
+
+        public bool GetWhiteKingsideCastlingAvailability(string fenSubstring)
+        {
+            if (fenSubstring.Contains("K"))
+            {
+                return true;
+            }
+            else if (fenSubstring.Contains("-"))
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public bool GetWhiteQueensideCastlingAvailability(string fenSubstring)
+        {
+            if (fenSubstring.Contains("Q"))
+            {
+                return true;
+            }
+            else if (fenSubstring.Contains("-"))
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public bool GetBlackKingsideCastlingAvailability(string fenSubstring)
+        {
+            if (fenSubstring.Contains("k"))
+            {
+                return true;
+            }
+            else if (fenSubstring.Contains("-"))
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public bool GetBlackQueensideCastlingAvailability(string fenSubstring)
+        {
+            if (fenSubstring.Contains("q"))
+            {
+                return true;
+            }
+            else if (fenSubstring.Contains("-"))
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public string GetEnPassantSquare(string fenSubstring)
+        {
+            if (fenSubstring.Contains("-"))
+            {
+                return String.Empty;
+            }
+            else if (!String.IsNullOrEmpty(fenSubstring))
+            {
+                return fenSubstring;
+            }
+
+            return String.Empty;
+        }
+
+        public int? GetHalfmoveClock(string fenSubstring)
+        {
+            int n;
+            return int.TryParse(fenSubstring, out n) ? int.Parse(fenSubstring) : (int?)null;
+        }
+
+        public int? GetFullmoveNumber(string fenSubstring)
+        {
+            int n;
+            return int.TryParse(fenSubstring, out n) ? int.Parse(fenSubstring) : (int?)null;
+        }
+
+        public FenParser()
+        {
+            FenString = new FenString();
+        }
+
+        /// <summary>
+        /// Loads a FenParser object with a FEN string.
+        /// </summary>
+        public FenParser(FenString fen)
+        {
+            FenString = fen;
+        }
+    }
+
+}
